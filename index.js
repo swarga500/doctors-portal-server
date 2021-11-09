aa81fe858e11a739da84b95a48da924b653e8e86
swarga500 <swarga500@gmail.com>
2021-11-09 17:44:00 +0600

    date fixed

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run(){
 
         app.get('/appointments', verifyToken, async (req,res) =>{
           const email = req.query.email;
-          const date = new Date(req.query.date).toLocaleDateString();
+          const date = req.query.date;
           const query = {email:email, date: date}
           
           const cursor = appointmentCollection.find(query);
